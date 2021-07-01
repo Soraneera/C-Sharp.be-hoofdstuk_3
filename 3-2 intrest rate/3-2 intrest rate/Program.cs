49c957622db789b6a09fb7f7d4a7cfd36abac97c
Soraneera <85293087+Soraneera@users.noreply.github.com>
2021-07-01 17:06:31 +0200

    Add files via upload

---
+++ 3-2 intrest rate/3-2 intrest rate/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _3_2_intrest_rate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //variables
+            Double EK, BK, LT;
+
+            //Getting Variables
+            Console.WriteLine("Wat is het beginkapitaal? ");
+            BK = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Wat is de looptijd in jaren? ");
+            LT = Convert.ToDouble(Console.ReadLine());
+
+            //calculations
+            EK = BK * (0.125 * LT);
+
+            //Result
+            Console.WriteLine("Het eindkapitaal bedraagt: " + Math.Round(EK, 2).ToString());
+        }
+    }
+}
