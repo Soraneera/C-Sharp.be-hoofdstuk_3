04258bc78318f1bf882322ab870bef1ac1fa0645
Soraneera <85293087+Soraneera@users.noreply.github.com>
2021-07-02 21:05:19 +0200

    Add files via upload

---
+++ 3-3 dag tot verjaardag/3-3 dag tot verjaardag/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _3_3_dag_tot_verjaardag
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime DayNow = DateTime.Today;
+            DateTime BDay = new DateTime(2021, 11, 29);
+            TimeSpan T = BDay - DayNow;
+            Console.WriteLine("dagen tot Dominiques verjaardag: {0}", T);
+            Console.ReadLine();
+        }
+    }
+}
